6da95dd4c7c04bfa6bb8861b77263f4b28d0d46a
RebecaBrandao1 <rjb@ic.ufal.br>
2021-01-15 10:53:55 -0300

    First commit

---
+++ src/Components/Header/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+
+//stateless
+const Header = () => (
+  <header>
+    <h2 className="title">Consumindo a API do GitHub</h2>
+  </header>
+);
+
+export default Header;
